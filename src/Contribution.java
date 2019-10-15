971d6e43a9a5ee54c4556b43650f773a69b60287
Jarrett Tranel <49590631+DoubleShotTranel@users.noreply.github.com>
2019-10-14 19:45:31 -0500

    Homework

--- src/Contribution.java
+++ src/Contribution.java
@@ -1,3 +1,4 @@
+package investmentCalculator;
 
 public class Contribution 
 {
--- src/Investment.java
+++ src/Investment.java
@@ -1,3 +1,5 @@
+package investmentCalculator;
+
 import java.util.ArrayList;
 
 public class Investment 
@@ -16,15 +18,51 @@ public class Investment
 	
 	double getCurrentValue(int afterHowManyMonth)
 	{
-		//return the current value of this investment taking
-		//time and an average 8% yearly or 0.67% monthly return
-		//over afterHowManyMonths number of months
+		
+		double value = 0;
+		
+		int size = this.theContributions.size() -1;
+		for (int j = 0; j < this.theContributions.size(); j++)
+		{
+			Contribution currCont= this.theContributions.get(j);
+			value = value + currCont.amount;
+			if (j < size)
+			{
+				Contribution nextCont = this.theContributions.get(j+1);
+				for (int i = currCont.monthNumber; i < nextCont.monthNumber; i++)
+				{
+					value = value * 1.067;
+					
+				}
+			}
+			else
+			{
+				for (int i = currCont.monthNumber; i <= afterHowManyMonth; i++)
+				{
+					value = value * 1.067;
+					
+				}
+			}		
+	
+		}
+		
+		return value;
 	}
+	
+
 	
 	double getTotalContributions()
 	{
+		double total = 0;
+		for (int i = 0; i < this.theContributions.size(); i++)
+		{
+			Contribution cont = this.theContributions.get(i);
+			total += cont.amount;
+		}
+		return total;
 		//return the total amount ever investment not taking into
 		//account interest.
 	}
+	
 	
 }
